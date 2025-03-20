b02c6fa3b3edc8eddff59f69d7af7d9ea1e427b9
argalusmp <argalus03@gmail.com>
2025-03-20 14:57:23 +0700

    add link embedd canva

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -49,7 +49,8 @@ const projectsData = [
   {
     id: 5,
     title: "Laravel Todo List",
-    description: "Todo List app web based that build in php framework which is Laravel",
+    description:
+      "Todo List app web based that build in php framework which is Laravel",
     image: "/images/projects/55.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/argalusmp/Todolist-Basic-Laravel",
@@ -58,7 +59,8 @@ const projectsData = [
   {
     id: 6,
     title: "Laptop Price Prediction",
-    description: "This project focuses on predicting laptop prices using datasets available on Kaggle. Laptop price predictions are important to help consumers make purchasing decisions.",
+    description:
+      "This project focuses on predicting laptop prices using datasets available on Kaggle. Laptop price predictions are important to help consumers make purchasing decisions.",
     image: "/images/projects/66.jpg",
     tag: ["All", "Machine Learning"],
     gitUrl: "https://github.com/argalusmp/laptop_price_prediction_ML",
@@ -67,7 +69,8 @@ const projectsData = [
   {
     id: 7,
     title: "Indonesian Sign Languages",
-    description: "This application is expected to be able to recognize and understand sign language with high accuracy, so that it can help bridge communication between deaf and non-disabled communities",
+    description:
+      "This application is expected to be able to recognize and understand sign language with high accuracy, so that it can help bridge communication between deaf and non-disabled communities",
     image: "/images/projects/sign_languages.jpg",
     tag: ["All", "Machine Learning"],
     gitUrl: "https://github.com/argalusmp/gestura_app",
@@ -76,7 +79,8 @@ const projectsData = [
   {
     id: 8,
     title: "IoT Waste Checking",
-    description: "This application is expected to be able to checking and organized waste or trash information, so can help us to how organized trash better",
+    description:
+      "This application is expected to be able to checking and organized waste or trash information, so can help us to how organized trash better",
     image: "/images/projects/trashcker.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/argalusmp/trashcker_IoT",
@@ -129,7 +133,7 @@ const ProjectsSection = () => {
           isSelected={tag === "Machine Learning"}
         />
       </div>
-      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12 mb-11">
         {filteredProjects.map((project, index) => (
           <motion.li
             key={index}
@@ -149,6 +153,50 @@ const ProjectsSection = () => {
           </motion.li>
         ))}
       </ul>
+
+      <div className="text-white flex flex-col justify-center items-center gap-2 py-6">
+        <div
+          style={{
+            position: "relative",
+            width: "100%",
+            height: "0",
+            paddingTop: "56.25%",
+            paddingBottom: "0",
+            boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)",
+            marginTop: "1.6em",
+            marginBottom: "0.9em",
+            overflow: "hidden",
+            borderRadius: "8px",
+            willChange: "transform",
+          }}
+        >
+          <iframe
+            loading="lazy"
+            style={{
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              top: "0",
+              left: "0",
+              border: "none",
+              padding: "0",
+              margin: "0",
+            }}
+            src="https://www.canva.com/design/DAGiPyJtZxA/6ZbqaJsbauNKVctmgPuoEQ/view?embed"
+            allowfullscreen="allowfullscreen"
+            allow="fullscreen"
+          ></iframe>
+        </div>
+        <a
+          href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGiPyJtZxA&#x2F;6ZbqaJsbauNKVctmgPuoEQ&#x2F;view?utm_content=DAGiPyJtZxA&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link"
+          target="_blank"
+          rel="noopener"
+          className="text-white underline "
+        >
+          My Portofolio in Slider
+        </a>{" "}
+       
+      </div>
     </section>
   );
 };
