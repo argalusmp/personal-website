6765eadcafccf8d7f39f997c21720064b4132056
argalusmp <argalus03@gmail.com>
2025-04-12 22:53:21 +0700

    change profil picture

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -65,7 +65,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#103666] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/profil22.png"
+              src="/images/Profil.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={150}
